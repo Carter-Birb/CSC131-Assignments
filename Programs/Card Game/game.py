61b578961e0671197f8b410a733d812b38deece5
Carter <colandry05@gmail.com>
2025-02-19 22:45:28 -0600

    Added more GUI Elements

--- Programs/Card Game/game.py
+++ Programs/Card Game/game.py
@@ -3,14 +3,15 @@ import CardGame
 
 WIDTH = 600
 HEIGHT = 600
-BG = "cadet blue"
+BGLABEL = "SteelBlue4"
+BGBUTTON = "midnight blue"
 FONT = "Outfit"
 
 class MainGUI(Frame):
     
     
     def __init__(self, parent):
-        super().__init__(parent, bg=BG)
+        super().__init__(parent, bg=BGLABEL)
         self.setupGUI()
     
     
@@ -20,28 +21,65 @@ class MainGUI(Frame):
             self,
             text="Computer Picked",
             anchor=NW,
-            bg=BG,
+            bg=BGLABEL,
             fg="white",
             font=(FONT, 10)
             )
-        self.L1.grid(row=0, column=0, sticky=NW)
+        self.L1.grid(row=0, column=0, sticky=NW, padx=5, pady=5)
         
         self.L2 = Label(
             self,
             text="You picked",
             anchor=NE,
-            bg=BG,
+            bg=BGLABEL,
             fg="white",
             font=(FONT, 10)
         )
-        self.L2.grid(row=0, column=6, sticky=NE)
+        self.L2.grid(row=0, column=10, sticky=NE, padx=5, pady=5)
         
-        for row in range(4):
+        self.Play = Button(
+            self,
+            text="Play",
+            bg=BGBUTTON,
+            fg="white",
+            highlightbackground=BGBUTTON,
+            width=5,
+            activebackground="light gray",
+            command=lambda: self.process("Play")
+        )
+        self.Play.grid(row=10, column=0, sticky=SW, padx=5, pady=5)
+        
+        self.Restart = Button(
+            self,
+            text="Restart",
+            bg=BGBUTTON,
+            fg="white",
+            highlightbackground=BGBUTTON,
+            width=5,
+            activebackground="light gray",
+            command=lambda: self.process("Restart")
+        )
+        self.Restart.grid(row=10, column=1, sticky=SW, padx=5, pady=5)
+        
+        self.Quit = Button(
+            self,
+            text="Quit",
+            bg=BGBUTTON,
+            fg="white",
+            highlightbackground=BGBUTTON,
+            width=5,
+            activebackground="light gray",
+            command=lambda: self.process("Quit")
+        )
+        self.Quit.grid(row=10, column=10, sticky=SE, padx=5, pady=5)
+        
+        
+        for row in range(10):
             Grid.rowconfigure(self, row, weight=1)
-        for col in range(6):
+        for col in range(10):
             Grid.columnconfigure(self, col, weight=1)
         
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=True)
     
     
     def process(self, button):
