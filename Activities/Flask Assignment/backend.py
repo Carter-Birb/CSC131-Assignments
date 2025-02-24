90e278b38f74d1024750b2db5d7fa713ab5f0bba
Carter <colandry05@gmail.com>
2025-02-24 16:56:23 -0600

    Added more pokemon, as well as created their own file

--- Activities/Flask Assignment/backend.py
+++ Activities/Flask Assignment/backend.py
@@ -6,59 +6,9 @@
 
 
 from flask import Flask, jsonify, request
+from pokedexentries import DEXENTRIES
 
 app = Flask(__name__)
-# As a little project, I may host a github repository to allow friends to add dex entries
-
-"""
-DEXENTRIES are of the form:
-
-pokemon name (lower): {
-    "name": "pokemon name",
-    "type": "pokemon type(s)",
-    "weakness": "pokemon weakness(es)",
-    "region": "pokemon region",
-    "description": "pokemon pokedex description/entry",
-    }
-"""
-
-DEXENTRIES = {
-    "pikachu": {
-        "name": "pikachu",
-        "type": "electric",
-        "weakness": "ground",
-        "region": "kanto",
-        "description": "This pokemon has electricity-storing pouches on its cheeks. These appear to become electrically charged during the night while pikachu sleeps. It occasionally discharges electricity when it is dozy after waking up. It has small electric sacs on both its cheeks.",
-    },
-    "lucario": {
-        "name": "lucario",
-        "type": "fighting/steel",
-        "weakness": "fighting, ground, fire",
-        "region": "sinnoh",
-        "description": "Lucario reads its opponent's feelings with its aura waves. It finds out things it would rather not know, so it gets stressed out easily. It controls waves known as auras, which are powerful enough to pulverize huge rocks. It uses these waves to take down its prey.",
-    },
-    "rayquaza": {
-        "name": "rayquaza",
-        "type": "flying/dragon",
-        "weakness": "fairy, rock, ice, dragon",
-        "region": "hoenn",
-        "description": "Rayquaza is said to have lived for hundreds of millions of years. Legends remain of how it put to rest the clash between Kyogre and Groudon. It flies forever through the ozone layer, consuming meteoroids for sustenance. The many meteoroids in its body provide the energy it needs to Mega Evolve.",
-    },
-    "ceruledge": {
-        "name": "ceruledge",
-        "type": "ghost/fire",
-        "weakness": "water, ground, rock",
-        "region": "paldea",
-        "description": "Ceruledge dons an old set of armor steeped in grudges and wields blades made of fire and ghost energy. In battle, these blades transform into great swords to increase Ceruledge’s power. Cuts from these great swords leave wounds from which life energy will flow.",
-    },
-    "charizard": {
-        "name": "charizard",
-        "type": "fire/flying",
-        "weakness": "water, rock, electric",
-        "region": "kanto",
-        "description": "It boasts speed and maneuverability greater than that of a jet fighter. The flame inside its body burns hotter than 3,600 degrees Fahrenheit. When Charizard roars, that temperature climbs even higher. This colossal, flame-winged figure of a Charizard was brought about by Gigantamax energy.",
-    },
-}
 
 @app.route("/", methods=["GET"])
 def home():
---
+++ Activities/Flask Assignment/pokedexentries.py
@@ -0,0 +1,142 @@
+# As a little project, I may host a github repository to allow friends to add dex entries
+
+"""
+DEXENTRIES are of the form:
+
+pokemon name (lower): {
+    "name": "pokemon name",
+    "type": "pokemon type(s)",
+    "weakness": "pokemon weakness(es)",
+    "region": "pokemon region",
+    "description": "pokemon pokedex description/entry",
+    }
+"""
+
+DEXENTRIES = {
+    "bulbasaur": {
+        "name": "bulbasaur",
+        "type": "grass/poison",
+        "weakness": "fire, psychic, flying, ice",
+        "region": "kanto",
+        "description": "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon."
+    },
+    "charmander": {
+        "name": "charmander",
+        "type": "fire",
+        "weakness": "water, ground, rock",
+        "region": "kanto",
+        "description": "Obviously prefers hot places. When it rains, steam is said to spout from the tip of its tail."
+    },
+    "cyndaquil": {
+        "name": "cyndaquil",
+        "type": "fire",
+        "weakness": "water, ground, rock",
+        "region": "johto",
+        "description": "It usually stays hunched over. If it is angry or surprised, it shoots flames out of its back."
+    },
+    "totodile": {
+        "name": "totodile",
+        "type": "water",
+        "weakness": "electric, grass",
+        "region": "johto",
+        "description": "It has the habit of biting anything with its developed jaws. Even its trainer needs to be careful."
+    },
+    "treecko": {
+        "name": "treecko",
+        "type": "grass",
+        "weakness": "fire, ice, poison, flying, bug",
+        "region": "hoenn",
+        "description": "It quickly scales even vertical walls. It senses humidity with its tail to predict the next day’s weather."
+    },
+    "torchic": {
+        "name": "torchic",
+        "type": "fire",
+        "weakness": "water, ground, rock",
+        "region": "hoenn",
+        "description": "It has a flame sac inside its belly that perpetually burns. It feels warm if it is hugged."
+    },
+    "piplup": {
+        "name": "piplup",
+        "type": "water",
+        "weakness": "electric, grass",
+        "region": "sinnoh",
+        "description": "Because it is very proud, it hates accepting food from people. Its thick down guards it from cold."
+    },
+    "lucario": {
+        "name": "lucario",
+        "type": "fighting/steel",
+        "weakness": "fire, fighting, ground",
+        "region": "sinnoh",
+        "description": "A well-trained one can sense auras to identify and take in the feelings of creatures over half a mile away."
+    },
+    "snivy": {
+        "name": "snivy",
+        "type": "grass",
+        "weakness": "fire, ice, poison, flying, bug",
+        "region": "unova",
+        "description": "Being exposed to sunlight makes its movements swifter. It uses vines more adeptly than its hands."
+    },
+    "zorua": {
+        "name": "zorua",
+        "type": "dark",
+        "weakness": "fighting, bug, fairy",
+        "region": "unova",
+        "description": "It changes into the forms of others to surprise them. Apparently, it often transforms into a silent child."
+    },
+    "froakie": {
+        "name": "froakie",
+        "type": "water",
+        "weakness": "electric, grass",
+        "region": "kalos",
+        "description": "It secretes flexible bubbles, which reduce the damage it takes when attacked."
+    },
+    "talonflame": {
+        "name": "talonflame",
+        "type": "fire/flying",
+        "weakness": "water, electric, rock",
+        "region": "kalos",
+        "description": "It soars through the sky at speeds over 310 mph, using its sharp claws to attack prey."
+    },
+    "rowlet": {
+        "name": "rowlet",
+        "type": "grass/flying",
+        "weakness": "fire, flying, poison, rock",
+        "region": "alola",
+        "description": "It uses photosynthesis to store up energy during the day while becoming active at night."
+    },
+    "mimikyu": {
+        "name": "mimikyu",
+        "type": "ghost/fairy",
+        "weakness": "ghost, steel",
+        "region": "alola",
+        "description": "Its actual appearance is unknown. A scholar who saw what was under its rag was overwhelmed by terror and died."
+    },
+    "scorbunny": {
+        "name": "scorbunny",
+        "type": "fire",
+        "weakness": "water, ground, rock",
+        "region": "galar",
+        "description": "It has special pads on the backs of its feet, and one kick from a Scorbunny can start a fire."
+    },
+    "dragapult": {
+        "name": "dragapult",
+        "type": "dragon/ghost",
+        "weakness": "ice, dragon, fairy, ghost, dark",
+        "region": "galar",
+        "description": "Dragapult habitually launches Dreepy at Mach speeds from the crests on its head."
+    },
+    "sprigatito": {
+        "name": "sprigatito",
+        "type": "grass",
+        "weakness": "fire, ice, poison, flying, bug",
+        "region": "paldea",
+        "description": "The fluffy fur absorbs water, allowing it to stay hydrated and battle in dry areas."
+    },
+    "ceruledge": {
+        "name": "ceruledge",
+        "type": "fire/ghost",
+        "weakness": "water, ground, rock, ghost, dark",
+        "region": "paldea",
+        "description": "Its fiery blades leave lingering wounds that burn for eternity. It fights in close combat without mercy."
+    }
+}
