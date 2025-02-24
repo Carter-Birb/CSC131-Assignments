50db8f6943c0516ee0e37da84d717b7d78463fc2
Carter <colandry05@gmail.com>
2025-02-24 13:12:56 -0600

    Flask Assignment basically finished

--- Activities/Flask Assignment/backend.py
+++ Activities/Flask Assignment/backend.py
@@ -1,62 +1,80 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+# As a little project, I will allow friends to add dex entries
+
+"""
+DEXENTRIES are of the form:
+
+pokemon name (lower): {
+    "name": "pokemon name (Title)",
+    "type": "pokemon type(s)",
+    "weakness": "pokemon weakness(es)",
+    "region": "pokemon region",
+    "description": "pokemon pokedex description/entry",
+    }
+"""
+DEXENTRIES = {
+    "pikachu": {
+        "name": "Pikachu",
+        "type": "electric",
+        "weakness": "ground",
+        "region": "kanto",
+        "description": "This pokemon has electricity-storing pouches on its cheeks. These appear to become electrically charged during the night while pikachu sleeps. It occasionally discharges electricity when it is dozy after waking up. It has small electric sacs on both its cheeks.",
+    },
+    "lucario": {
+        "name": "Lucario",
+        "type": "fighting/steel",
+        "weakness": "fighting, ground, fire",
+        "region": "sinnoh",
+        "description": "Lucario reads its opponent's feelings with its aura waves. It finds out things it would rather not know, so it gets stressed out easily. It controls waves known as auras, which are powerful enough to pulverize huge rocks. It uses these waves to take down its prey.",
+    },
+    "rayquaza": {
+        "name": "Rayquaza",
+        "type": "flying/dragon",
+        "weakness": "fairy, rock, ice, dragon",
+        "region": "hoenn",
+        "description": "Rayquaza is said to have lived for hundreds of millions of years. Legends remain of how it put to rest the clash between Kyogre and Groudon. It flies forever through the ozone layer, consuming meteoroids for sustenance. The many meteoroids in its body provide the energy it needs to Mega Evolve.",
+    },
+    "ceruledge": {
+        "name": "Ceruledge",
+        "type": "ghost/fire",
+        "weakness": "water, ground, rock",
+        "region": "paldea",
+        "description": "Ceruledge dons an old set of armor steeped in grudges and wields blades made of fire and ghost energy. In battle, these blades transform into great swords to increase Ceruledge’s power. Cuts from these great swords leave wounds from which life energy will flow.",
+    },
+    "charizard": {
+        "name": "Charizard",
+        "type": "fire/flying",
+        "weakness": "water, rock, electric",
+        "region": "kanto",
+        "description": "It boasts speed and maneuverability greater than that of a jet fighter. The flame inside its body burns hotter than 3,600 degrees Fahrenheit. When Charizard roars, that temperature climbs even higher. This colossal, flame-winged figure of a Charizard was brought about by Gigantamax energy.",
+    },
+}
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"success": True, "me": "Carter"})
 
-@app.route("/pikachu", methods=["GET"])
-def pikachu():
-    entry = {
-        "type": "electric",
-        "weakness": "ground",
-        "region": "kanto",
-        "description": "This pokemon has electricity-storing pouches on its cheeks. These appear to become electrically charged during the night while pikachu sleeps. It occasionally discharges electricity when it is dozy after waking up. It has small electric sacs on both its cheeks.",
-    }
-    data = jsonify(entry)
-    return data
 
-@app.route("/lucario", methods=["GET"])
-def lucario():
-    entry = {
-        "type": "fighting/steel",
-        "weakness": "fighting, ground, fire",
-        "region": "sinnoh",
-        "description": "Lucario reads its opponent's feelings with its aura waves. It finds out things it would rather not know, so it gets stressed out easily. It controls waves known as auras, which are powerful enough to pulverize huge rocks. It uses these waves to take down its prey.",
-    }
-    data = jsonify(entry)
-    return data
+# Returns the pokemon within the pokedex when the program is run. This updates dynamically with pokemon added.
+@app.route("/pokedexlist", methods=["GET"])
+def pokedexlist():
+    """
+    returns the pokemon currently in the pokedex (keys)
+    """
+    return jsonify(list(DEXENTRIES.keys()))
 
-@app.route("/rayquaza", methods=["GET"])
-def rayquaza():
-    entry = {
-        "type": "flying/dragon",
-        "weakness": "fairy, rock, ice, dragon",
-        "region": "hoenn",
-        "description": "Rayquaza is said to have lived for hundreds of millions of years. Legends remain of how it put to rest the clash between Kyogre and Groudon. It flies forever through the ozone layer, consuming meteoroids for sustenance. The many meteoroids in its body provide the energy it needs to Mega Evolve.",
-    }
-    data = jsonify(entry)
-    return data
 
-@app.route("/ceruledge", methods=["GET"])
-def ceruledge():
-    entry = {
-        "type": "ghost/fire",
-        "weakness": "water, ground, rock",
-        "region": "paldea",
-        "description": "Ceruledge dons an old set of armor steeped in grudges and wields blades made of fire and ghost energy. In battle, these blades transform into great swords to increase Ceruledge’s power. Cuts from these great swords leave wounds from which life energy will flow.",
-    }
-    data = jsonify(entry)
-    return data
+# This creates a dynamic endpoint system that will update with pokemon added to the DEXENTRIES dictionary.
+@app.route("/<pokemon>", methods=["GET"])
+def getpokemon(pokemon:str):
+    """
+    returns the information from a given pokemon
+    """
+    if pokemon in DEXENTRIES:
+        return jsonify(DEXENTRIES[pokemon])
 
-@app.route("/charizard", methods=["GET"])
-def charizard():
-    entry = {
-        "type": "fire/flying",
-        "weakness": "water, rock, electric",
-        "region": "kanto",
-        "description": "It boasts speed and maneuverability greater than that of a jet fighter. The flame inside its body burns hotter than 3,600 degrees Fahrenheit. When Charizard roars, that temperature climbs even higher. This colossal, flame-winged figure of a Charizard was brought about by Gigantamax energy.",
-    }
-    data = jsonify(entry)
-    return data
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="8000")
--- Activities/Flask Assignment/frontend.py
+++ Activities/Flask Assignment/frontend.py
@@ -1,4 +1,6 @@
+from time import sleep
 import requests
+import backend
 
 class Pokedex:
     
@@ -6,17 +8,57 @@ class Pokedex:
     API_URL = f"http://{IP_ADDR}:8000"
     
     def __init__(self):
-        self.message = ""
-        self.entry = [] # each index in this list will have a slot for information about the pokemon
+        pokedexlist = requests.get(url=f"{Pokedex.API_URL}/pokedexlist")
+        self.pokedexlistdata = pokedexlist.json()
     
     
     def startdex(self):
-        pass
+        print("Welcome to the Pokedex!")
+        sleep(1)
+        print("Valid commands are: \"<pokemon>\" \"entries\" \"help\" \"quit\"")
+        sleep(1)
+        print("The current Pokemon in the Pokedex are:")
+        sleep(1)
+        print(", ".join(self.pokedexlistdata))
+        sleep(1)
+        print("Which pokemon would you like to know more about?")
+        Pokedex.rundex(self)
     
     
-    def getentry(self):
-        pass
+    def getentry(self, pokemon):
+            entry = requests.get(url=f"{Pokedex.API_URL}/{pokemon}")
+            entrydata = entry.json()
+            return f"\nName: {entrydata['name']}\nType: {entrydata['type']}\nWeakness: {entrydata['weakness']}\nRegion: {entrydata['region']}\nDescription: {entrydata['description']}\n"
     
     
     def rundex(self):
-        pass
+        while True:
+            userinput = input("").lower().strip()
+            
+            if userinput in self.pokedexlistdata:
+                print(Pokedex.getentry(self, userinput))
+                sleep(1)
+                
+            elif userinput == "entries":
+                print("The current Pokemon in the Pokedex are:")
+                sleep(1)
+                print(", ".join(self.pokedexlistdata))
+                sleep(1)
+                
+            elif userinput == "help":
+                print("Valid commands are: \"<pokemon>\" \"entries\" \"help\"")
+                sleep(1)
+                
+            elif userinput == "quit":
+                exit()
+            else:
+                print("That pokemon is not in the Pokedex")
+                sleep(1)
+            
+            print("Which pokemon would you like to know more about?")
+
+
+
+if __name__ == "__main__":
+    p = Pokedex()
+    p.startdex()
